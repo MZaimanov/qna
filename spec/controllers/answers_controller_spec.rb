3aff5c28154caa40bc2859fffb82303e77d738ac
MZaimanov(mike) <mizaimanov@gmail.com>
2018-08-14 16:59:24 +0300

    Пользователь может создавать ответы

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -5,22 +5,22 @@ RSpec.describe AnswersController, type: :controller do
     let(:question) { create :question }
     context 'Создание валидного объекта с правильными атрибутами'do
       it 'Сохранение нового вопроса в БД' do
-        expect { post :create, params: { answer: attributes_for(:answer), question_id: question} }.to change(question.answers, :count).by(1)
+        expect { post :create, params: { answer: attributes_for(:answer), question_id: question, format: :js } }.to change(question.answers, :count).by(1)
       end
       it 'redirect to question show view' do
-        post :create, params: { answer: attributes_for(:answer), question_id: question }
-        expect(response).to redirect_to question_path(question)
+        post :create, params: { answer: attributes_for(:answer), question_id: question, format: :js }
+        expect(response.body).to be_blank
       end
     end
 
     context 'Передача не верных параметров' do
       it 'Новый вопрос НЕ СОХРОНЯЕТСЯ' do
-        expect { post :create, params: { answer: attributes_for(:invalid_answer), question_id: question } }.to_not change(Answer, :count)
+        expect { post :create, params: { answer: attributes_for(:invalid_answer), question_id: question, format: :js } }.to_not change(Answer, :count)
       end
 
       it 'redirect to question show view' do
-        post :create, params: { answer: attributes_for(:invalid_answer), question_id: question }
-        expect(response).to redirect_to question_path(question)
+        post :create, params: { answer: attributes_for(:invalid_answer), question_id: question, format: :js }
+        expect(response).to render_template :create
       end
     end
   end
